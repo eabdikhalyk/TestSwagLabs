174d989d4371980ba6b8edecc6f1f11a0c2a5859
eabdikhalyk <eabdikhalyk@ail.ru>
2022-06-10 09:54:13 +0500

    test commit

---
+++ test/auth.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+browser = webdriver.Chrome(r"C:\Users\yakp\PycharmProjects\pythonProject\web_driver\chromedriver.exe")
+
+def test_start():
+    browser.get("https://www.saucedemo.com/")
+
+def test_auth():
+    user_name = "standard_user"
+    password = "secret_sauce"
+    title_name = "Products"
+
+    user_name_field = browser.find_element(By.ID,"user-name")
+    user_name_field.click()
+    user_name_field.send_keys(user_name)
+
+    password_field = browser.find_element(By.ID,"password")
+    password_field.click()
+    password_field.send_keys(password)
+
+    login = browser.find_element(By.ID,"login-button")
+    login.click()
+
+    title = browser.find_element(By.CLASS_NAME,"title")
+
+    assert title.text.lower() == title_name.lower()
+
+
+
